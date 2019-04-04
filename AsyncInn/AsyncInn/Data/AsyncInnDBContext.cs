ccfcbe225fa784156788f2002ce064ff55abef25
daniel.logerstedt@gmail.com <daniel.logerstedt@gmail.com>
2019-04-04 12:11:30 -0700

    Built out DB Context and Startup

---
+++ AsyncInn/AsyncInn/Data/AsyncInnDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AsyncInn.Models;
+
+namespace AsyncInn.Data
+{
+    public class AsyncInnDBContext : DbContext
+    {
+        public AsyncInnDBContext(DbContextOptions<AsyncInnDBContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        // TODO: Create Tables
+    }
+}
